ae4a9545b10e3af8c47e4ea92153f437293a81ad
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2021-11-08 22:31:32 -0800

    feat: Add top nav, customize favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,9 +5,28 @@ const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let  header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <primaryNav>
+        <h1 className="main-heading">
+          <Link to="/">{title}</Link>
+        </h1>
+        <ul className="main-heading-links">
+          <li>
+            ➡️ <Link to="/">
+              Read the blog
+            </Link>
+          </li>
+          <li>
+            ➡️ <Link to="/about">
+              About the show
+            </Link>
+          </li>
+          <li>
+            ➡️ <Link to="https://cubecobra.com/cube/overview/clockspinning">
+              Check out the cube
+            </Link>
+          </li>
+        </ul>
+      </primaryNav>
     )
 
   return (
